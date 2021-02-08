71ef3f74bdaa7273d319748264c1c6b15bf2998f
Andy Vo <andyvozx@gmail.com>
2021-02-08 17:51:50 -0600

    added styles and pic

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-let image = document.getElementsByClassName("flex-item");
+let count = 0;
+let property = document.getElementById("parent");
 
-function changeImage() {
-  if (image.getAttribute("src") == "circleRed.png") {
-    image.src = "circleBlue.png";
-  } else {
-    image.src = "circleRed.png";
-  }
+function Start() {
+  document.getElementById("1").style.background = "red";
 }
+
+window.onload = function () {
+  document.getElementById("1").onclick = function click() {
+    console.log("click");
+    if (count == 0) {
+      document.getElementById("1").style.background =
+        "url('resources/pics/miguel.jpg')";
+      count = 1;
+    } else {
+      document.getElementById("1").style.background = "red";
+      count = 0;
+    }
+    console.log(count);
+  };
+};
