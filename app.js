6b874a5333cfd524af926477f1bb47d1fa152247
Andy Vo <andyvozx@gmail.com>
2021-02-09 06:00:26 -0600

    js if

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-let count = 0;
-let property = document.getElementById("parent");
-
 function Start() {
   document.getElementById("1").style.background = "red";
   document.getElementById("2").style.background = "red";
@@ -13,19 +10,127 @@ function Start() {
   document.getElementById("9").style.background = "red";
 }
 
+// let pic2 = document.getElementById("2");
+// let pic3 = document.getElementById("3");
+// let pic4 = document.getElementById("4");
+// let pic5 = document.getElementById("5");
+// let pic6 = document.getElementById("6");
+// let pic7 = document.getElementById("7");
+// let pic8 = document.getElementById("8");
+// let pic9 = document.getElementById("9");
+
 window.onload = function () {
-  document.getElementById("1").onclick = function click() {
-    console.log("click");
+  let count = 0;
+  let pic1 = document.getElementById("1");
+  pic1.onclick = function click() {
     if (count == 0) {
-      document.getElementById("1").style.background =
-        "url('resources/pics/miguel.jpg')";
+      pic1.style.background = "url('resources/pics/andy.jpg')";
       count = 1;
     } else {
-      document.getElementById("1").style.background = "red";
+      pic1.style.background = "red";
       count = 0;
     }
     console.log(count);
   };
 };
 
-// 2-9
+// clean up
+
+function toggle2() {
+  let count = 0;
+  let pic = document.getElementById("2");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/alex.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
+
+function toggle3() {
+  let count = 0;
+  let pic = document.getElementById("3");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/andy.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
+
+function toggle4() {
+  let count = 0;
+  let pic = document.getElementById("4");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/alex.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
+function toggle5() {
+  let count = 0;
+  let pic = document.getElementById("5");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/andy.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
+function toggle6() {
+  let count = 0;
+  let pic = document.getElementById("6");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/miguel.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
+function toggle7() {
+  let count = 0;
+  let pic = document.getElementById("7");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/alex.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
+function toggle8() {
+  let count = 0;
+  let pic = document.getElementById("8");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/alex.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
+function toggle9() {
+  let count = 0;
+  let pic = document.getElementById("9");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/miguel.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
