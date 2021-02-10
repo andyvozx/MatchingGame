946a16c1c85c325ff120688d2cd968ef70b40012
Andy Vo <andyvozx@gmail.com>
2021-02-10 07:35:06 -0600

    added js and more divs

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Start Button Init
+
 function Start() {
   document.getElementById("1").style.background = "red";
   document.getElementById("2").style.background = "red";
@@ -9,15 +11,6 @@ function Start() {
   document.getElementById("8").style.background = "red";
   document.getElementById("9").style.background = "red";
 }
-
-// let pic2 = document.getElementById("2");
-// let pic3 = document.getElementById("3");
-// let pic4 = document.getElementById("4");
-// let pic5 = document.getElementById("5");
-// let pic6 = document.getElementById("6");
-// let pic7 = document.getElementById("7");
-// let pic8 = document.getElementById("8");
-// let pic9 = document.getElementById("9");
 
 window.onload = function () {
   let count = 0;
@@ -34,7 +27,20 @@ window.onload = function () {
   };
 };
 
-// clean up
+function Win() {
+  Start();
+  document.getElementById("container3").style.flexDirection = "row-reverse";
+  let container1 = document.getElementById("container1");
+  let container2 = document.getElementById("container2");
+  let container3 = document.getElementById("container3");
+
+  let items = [container1, container2, container3];
+  for (let i = 0; i < items.length; i++) {
+    let target = Math.floor(Math.random() * items.length - 1) + 1;
+  }
+}
+
+// Toggle Pics
 
 function toggle2() {
   let count = 0;
@@ -92,11 +98,7 @@ function toggle6() {
   let count = 0;
   let pic = document.getElementById("6");
   if (count === 0) {
-    pic.style.background = "url('resources/pics/miguel.jpg')";
-    setTimeout(function () {
-      alert("You found Miguel. ICE is on the way!");
-      Start();
-    }, 500);
+    pic.style.background = "url('resources/pics/andy.jpg')";
     count = 1;
   } else {
     pic.style.background = "red";
@@ -105,11 +107,22 @@ function toggle6() {
   console.log(count);
 }
 
+// toggle 7 is Win
+
 function toggle7() {
   let count = 0;
   let pic = document.getElementById("7");
   if (count === 0) {
-    pic.style.background = "url('resources/pics/alex.jpg')";
+    pic.style.background = "url('resources/pics/miguel.jpg')";
+    setTimeout(function () {
+      let audio = new Audio("resources/sounds/heli.mp3");
+
+      //Helicopter sounds
+
+      audio.play();
+      alert("You found Miguel. ICE is on the way!");
+      Win();
+    }, 500);
     count = 1;
   } else {
     pic.style.background = "red";
@@ -135,11 +148,44 @@ function toggle9() {
   let count = 0;
   let pic = document.getElementById("9");
   if (count === 0) {
-    pic.style.background = "url('resources/pics/miguel.jpg')";
-    setTimeout(function () {
-      alert("You found Miguel. ICE is on the way!");
-      Start();
-    }, 500);
+    pic.style.background = "url('resources/pics/alex.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
+
+function toggle10() {
+  let count = 0;
+  let pic = document.getElementById("10");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/alex.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
+function toggle11() {
+  let count = 0;
+  let pic = document.getElementById("11");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/andy.jpg')";
+    count = 1;
+  } else {
+    pic.style.background = "red";
+    count = 0;
+  }
+  console.log(count);
+}
+function toggle12() {
+  let count = 0;
+  let pic = document.getElementById("12");
+  if (count === 0) {
+    pic.style.background = "url('resources/pics/alex.jpg')";
     count = 1;
   } else {
     pic.style.background = "red";
