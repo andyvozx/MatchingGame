7271ef1b7ba964ff247ee675c2332e254fb58cc3
Andy Vo <andyvozx@gmail.com>
2021-02-10 17:04:54 -0600

    added score subtraction

--- app.js
+++ app.js
@@ -13,9 +13,15 @@ function Start() {
   document.getElementById("10").style.background = "red";
   document.getElementById("11").style.background = "red";
   document.getElementById("12").style.background = "red";
-
+  // Hide Start button after game starts
   document.getElementById("startButton").style.display = "none";
 }
+
+// Audio
+
+let chaching = new Audio("resources/sounds/chaching.mp3");
+let heli = new Audio("resources/sounds/heli.mp3");
+let laugh = new Audio("resources/sounds/laugh.mp3");
 
 // total Score function
 
@@ -23,44 +29,57 @@ let totalScore = 0;
 
 function AddScore() {
   totalScore++;
-  document.getElementById("score").innerHTML = totalScore + " Miguel Caught";
+  document.getElementById("score").innerHTML = totalScore + " Miguel Captured";
+
+  if (totalScore < 0) {
+    let negitiveScore = totalScore * -1;
+    document.getElementById("score").innerHTML =
+      negitiveScore + " Miguel Escaped";
+  }
 }
 
-function MedScore() {
-  if (totalScore > 1)
-    document.getElementById("score").innerHTML = totalScore + " Miguels Caught";
+function SubScore() {
+  totalScore--;
+  document.getElementById("score").innerHTML = totalScore + " Miguels Captured";
+  document.getElementById("score").style.color = "red";
+
+  if (totalScore === 0 || totalScore === 1) {
+    document.getElementById("score").innerHTML =
+      totalScore + " Miguel Captured";
+  }
+  if (totalScore < 0) {
+    let negitiveScore = totalScore * -1;
+    document.getElementById("score").innerHTML =
+      negitiveScore + " Miguel Escaped";
+  }
 }
 
 function HighScore() {
-  if (totalScore > 3) document.getElementById("score").style.color = "#FFD700";
+  if (totalScore > 1) {
+    document.getElementById("score").innerHTML =
+      totalScore + " Miguels Captured";
+  }
+  if (totalScore === 4) {
+    heli.play();
+  }
+  if (totalScore > 3) {
+    document.getElementById("score").style.color = "#FFD700";
+    heli.play();
+  }
 }
-// window.onload = function () {
-//   let count = 0;
-//   let pic1 = document.getElementById("1");
-//   pic1.onclick = function click() {
-//     if (count == 0) {
-//       pic1.style.background = "url('resources/pics/andy.jpg')";
-//       count = 1;
-//     } else {
-//       pic1.style.background = "red";
-//       count = 0;
-//     }
-//     console.log(count);
-//   };
-// };
+
+// makes random number for changing pic position
+
+function randomNum(maxLimit = 12) {
+  let randomNum = Math.random() * maxLimit;
+  return Math.floor(randomNum);
+}
 
 function Win() {
   Start();
   AddScore();
-  MedScore();
   HighScore();
-
-  // makes random number for changing pic position
-
-  function randomNum(maxLimit = 12) {
-    let randomNum = Math.random() * maxLimit;
-    return Math.floor(randomNum);
-  }
+  randomNum();
 
   document.getElementById("1").style.order = randomNum();
   document.getElementById("2").style.order = randomNum();
@@ -76,6 +95,24 @@ function Win() {
   document.getElementById("12").style.order = randomNum();
 }
 
+function Lose() {
+  Start();
+  SubScore();
+  randomNum();
+
+  document.getElementById("1").style.order = randomNum();
+  document.getElementById("2").style.order = randomNum();
+  document.getElementById("3").style.order = randomNum();
+  document.getElementById("4").style.order = randomNum();
+  document.getElementById("5").style.order = randomNum();
+  document.getElementById("6").style.order = randomNum();
+  document.getElementById("7").style.order = randomNum();
+  document.getElementById("8").style.order = randomNum();
+  document.getElementById("9").style.order = randomNum();
+  document.getElementById("10").style.order = randomNum();
+  document.getElementById("11").style.order = randomNum();
+  document.getElementById("12").style.order = randomNum();
+}
 // Toggle Pics
 
 function toggle1() {
@@ -164,10 +201,8 @@ function toggle7() {
   if (count === 0) {
     pic.style.background = "url('resources/pics/miguel.jpg')";
     setTimeout(function () {
-      //Helicopter sounds
-      let audio = new Audio("resources/sounds/heli.mp3");
-      audio.play();
-      //alert("You found Miguel. ICE is on the way!");
+      // Winning sounds
+      chaching.play();
       Win();
     }, 700);
     count = 1;
@@ -191,11 +226,18 @@ function toggle8() {
   console.log(count);
 }
 
+// Toggle 9 to lose
+
 function toggle9() {
   let count = 0;
   let pic = document.getElementById("9");
   if (count === 0) {
-    pic.style.background = "url('resources/pics/alex.jpg')";
+    pic.style.background = "url('resources/pics/goblin.jpg')";
+    setTimeout(function () {
+      // Lose sounds
+      laugh.play();
+      Lose();
+    }, 700);
     count = 1;
   } else {
     pic.style.background = "red";
@@ -216,6 +258,7 @@ function toggle10() {
   }
   console.log(count);
 }
+
 function toggle11() {
   let count = 0;
   let pic = document.getElementById("11");
@@ -228,6 +271,7 @@ function toggle11() {
   }
   console.log(count);
 }
+
 function toggle12() {
   let count = 0;
   let pic = document.getElementById("12");
